0fd4efc8dcad27cdd5b3e85f55ead15a3f97fd08
Sasha Barvynska <alexbarvynska@gmail.com>
2022-09-09 22:37:17 +0300

    improvements by comments

--- task_1.py
+++ task_1.py
@@ -1,27 +1,17 @@
-if __name__ ==  '__main__':
-    cases = [("abcd efgh", "dcba hgfe"), ("a1bcd efg!h", "d1cba hgf!e"), ("", "")]
-    
-    def your_function(text):
-        str_new = ""
-        for word in text.split():
-            str_new += reverse_word(word) + " "
-        return str_new[:-1]
-        
-    def reverse_word(word):
-        list_new = list(word)
-        start_index = 0
-        end_index = len(list_new) - 1
-        while start_index < end_index:
-            if not list_new[start_index].isalpha():
-                start_index += 1
-            elif not list_new[end_index].isalpha():
-                end_index -= 1
-            else:
-                list_new[start_index], list_new[end_index] = list_new[end_index], list_new[start_index]
-                start_index += 1
-                end_index -= 1
-        return ''.join(list_new)
-    for text, reversed_text in cases:
-        assert your_function(text) == reversed_text
+def reverse_sentence(sentence: str) -> str:
 
-   
+    return " ".join(map(lambda x: reverse_word(x), sentence.split()))
+
+
+def reverse_word(word: str) -> str:
+
+    char_only = list(filter(lambda x: x.isalpha(), word))
+    str_star = "".join([char if char.isalpha() == False else "*" for char in word])
+    return "".join([char_only.pop() if char == "*" else char for char in str_star])
+
+
+if __name__ == "__main__":
+    cases = [("abcd efgh", "dcba hgfe"), ("a1bcd efg!h", "d1cba hgf!e"), ("", "")]
+
+    for text, reversed_text in cases:
+        assert reverse_sentence(text) == reversed_text
