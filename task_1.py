3d9faea73cbc63241094c89485e93daff58ef332
Sasha Barvynska <alexbarvynska@gmail.com>
2022-09-12 21:06:31 +0300

    Optimize solution

--- task_1.py
+++ task_1.py
@@ -1,17 +1,14 @@
 def reverse_sentence(sentence: str) -> str:
-
-    return " ".join(map(lambda x: reverse_word(x), sentence.split()))
+    return " ".join(map(reverse_word, sentence.split()))
 
 
 def reverse_word(word: str) -> str:
-
-    char_only = list(filter(lambda x: x.isalpha(), word))
-    str_star = "".join([char if char.isalpha() == False else "*" for char in word])
-    return "".join([char_only.pop() if char == "*" else char for char in str_star])
+    char_only = list(filter(str.isalpha, word))
+    return "".join([char if not char.isalpha() else char_only.pop() for char in word])
 
 
 if __name__ == "__main__":
-    cases = [("abcd efgh", "dcba hgfe"), ("a1bcd efg!h", "d1cba hgf!e"), ("", "")]
+    cases = [("abcd efgh!", "dcba hgfe!"), ("a1bcd efg!h", "d1cba hgf!e"), ("", "")]
 
     for text, reversed_text in cases:
         assert reverse_sentence(text) == reversed_text
